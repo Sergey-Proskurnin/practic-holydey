de6c9f3979e40fa274f53c69541f3c230d78f939
Sergey-Proskurnin <proskurnin@ua.fm>
2021-03-27 19:54:10 +0200

    Update menu-cards.js

--- src/js/menu-cards.js
+++ src/js/menu-cards.js
@@ -33,3 +33,31 @@ function inputChangeOn({ target }) {
     ? changeTheme(Theme.DARK, Theme.LIGHT)
     : changeTheme(Theme.LIGHT, Theme.DARK);
 }
+
+// import menu from '../data/menu.json';
+// import menuItemTemplate from '../template/template.hbs';
+
+
+// const menuRef = document.querySelector('.js-menu');
+// const themeRef = document.querySelector('input[id="theme-switch-toggle"]');
+// const bodyRef = document.body;
+// const Theme = {
+//   LIGHT: 'light-theme',
+//   DARK: 'dark-theme',
+// };
+// const menuTemplate = menuItemTemplate(menu)
+// menuRef.innerHTML = menuTemplate;
+// const theme = localStorage.getItem('theme');
+// bodyRef.classList.add(theme);
+// if (bodyRef.classList.contains(Theme.DARK)) {
+//     themeRef.checked = true
+// } if (bodyRef.classList.contains(Theme.LIGHT)) {
+//     themeRef.checked = false
+// };
+// function themeChange() {
+//     bodyRef.classList.add(Theme.DARK);
+//     bodyRef.classList.add(Theme.LIGHT);
+//     themeRef.checked === true ? bodyRef.classList.toggle(Theme.LIGHT) : bodyRef.classList.toggle(Theme.DARK);
+//     localStorage.setItem('theme', bodyRef.classList);
+// };
+// themeRef.addEventListener('click', themeChange);
