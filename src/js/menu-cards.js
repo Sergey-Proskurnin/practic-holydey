0bf332dbf703fe8ffa8f36d2ebb2c7899d9f5a9b
Sergey-Proskurnin <proskurnin@ua.fm>
2021-03-27 15:51:37 +0200

    Add project

---
+++ src/js/menu-cards.js
@@ -0,0 +1,35 @@
+import { templates } from 'handlebars';
+import data from '../data/menu.json';
+import template from '../template/template.hbs';
+
+const refs = {
+  body: document.body,
+  list: document.querySelector('.js-menu'),
+  input: document.querySelector('.theme-switch__toggle'),
+};
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+refs.body.classList.add(
+  localStorage.getItem('theme') === null
+    ? Theme.LIGHT
+    : localStorage.getItem('theme'),
+);
+refs.input.checked = localStorage.getItem('theme') === Theme.DARK;
+
+const markup = template(data);
+
+refs.list.insertAdjacentHTML('beforeend', markup);
+refs.input.addEventListener('change', inputChangeOn);
+
+function changeTheme(add, rem) {
+  refs.body.classList.replace(rem, add);
+  localStorage.setItem('theme', add);
+}
+
+function inputChangeOn({ target }) {
+  target.checked
+    ? changeTheme(Theme.DARK, Theme.LIGHT)
+    : changeTheme(Theme.LIGHT, Theme.DARK);
+}
